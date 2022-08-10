01cbb4e0f32b864d2f2ca768429aae07b00124fb
AmreshTripathy <amreshtripathy184@gmail.com>
2022-08-10 21:54:07 +0530

    date_10-08-2022

---
+++ src/main/java/com/practice/java_practice/FindPercentage.java
@@ -0,0 +1,24 @@
+package com.practice.java_practice;
+/*
+ * @Amresh Tripathy
+ */
+
+import java.util.Scanner;
+
+import org.apache.log4j.Logger;
+
+public class FindPercentage {
+	
+	static final Logger log = Logger.getLogger("FindPercentage.class");
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		int sum = 0;
+		Scanner sc = new Scanner(System.in);
+		for(int i=1; i<=5; i++ ) {
+			log.debug("Insert No."+i);
+			sum += sc.nextInt();
+		}
+		float percent = (sum * 100) / 500;
+		log.debug("Percentage is "+ percent);
+	}
+}
