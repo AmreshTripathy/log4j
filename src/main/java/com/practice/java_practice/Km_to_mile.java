ae54ad4a654a208eecf49adf3db5d1680c882c60
AmreshTripathy <amreshtripathy184@gmail.com>
2022-08-13 20:49:24 +0530

    modified_1

--- src/main/java/com/practice/java_practice/Km_to_mile.java
+++ src/main/java/com/practice/java_practice/Km_to_mile.java
@@ -14,6 +14,7 @@ public class Km_to_mile {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int kilometer = sc.nextInt();
+		//amresh
 		log.debug(kilometer * 0.621371);
 	}
 }
