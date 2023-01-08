5ae6e3fa6cfe9dfb26fb40f84b016226bffb8114
AmreshTripathy <amreshtripathy184@gmail.com>
2023-01-08 23:12:49 +0530

    Stack: Smallest Number Following Pattern https://course.acciojob.com/idle?question=efcb1e58-c615-48b1-a7ed-def039965808

---
+++ src/main/java/com/practice/kunalkhuswah_DSA/SmallestNumberFollowingPattern.java
@@ -0,0 +1,38 @@
+package com.practice.kunalkhuswah_DSA;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+/*
+ * @Amresh Tripathy
+ */
+
+public class SmallestNumberFollowingPattern {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		sc.close();
+		System.out.println(smallestNumber(str));
+	}
+
+	private static String smallestNumber(String str) {
+		Stack<Integer> st = new Stack<Integer>();
+		String ans = "";
+		int count = 1;
+		for(int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if(ch == 'd')
+				st.push(count++);
+			else {
+				st.push(count++);
+				while(st.size() > 0) 
+					ans += st.pop();
+			}
+		}
+		st.push(count++);
+		while(st.size() > 0) 
+			ans += st.pop();
+		
+		return ans;
+	}
+}
