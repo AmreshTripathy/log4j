a26bdb4916f28eb00459a6ddaeed177e5248894e
AmreshTripathy <amreshtripathy184@gmail.com>
2023-01-09 22:54:19 +0530

    Stack: 132 Pattern https://course.acciojob.com/idle?question=082fec26-a7a3-4296-adc5-2f3fe9d99f12 https://leetcode.com/problems/132-pattern/submissions/874870214/

--- src/main/java/com/practice/kunalkhuswah_DSA/OneThreeTwoPattern.java
+++ src/main/java/com/practice/kunalkhuswah_DSA/OneThreeTwoPattern.java
@@ -1,6 +1,7 @@
 package com.practice.kunalkhuswah_DSA;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 /*
  * @Amresh Tripathy
@@ -9,35 +10,37 @@ import java.util.Scanner;
 public class OneThreeTwoPattern {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-        int n;
-        n = sc.nextInt();
-        int arr[] = new int[n];
-        for (int i = 0; i < n; i++)
-            arr[i] = sc.nextInt();
-        
-        boolean result = find132pattern(arr);
-        if (result)
-            System.out.println("true");
-        else
-            System.out.println("false");
-        sc.close();
+		int n;
+		n = sc.nextInt();
+		int arr[] = new int[n];
+		for (int i = 0; i < n; i++)
+			arr[i] = sc.nextInt();
+
+		boolean result = find132pattern(arr);
+		if (result)
+			System.out.println("true");
+		else
+			System.out.println("false");
+		sc.close();
 	}
-	
+
 	private static boolean find132pattern(int[] nums) {
-       
-       int n = nums.length;
-       for(int i = 0; i < n-1; i++) {
-//    	   if(nums[i-1] < nums[i] && nums[i] > nums[i+1]) {
-//    		   return true;
-//    	   }
-    	   for(int j = i+1; j < n; j++) {
-    		   for(int k = j+1; k < n; k++) {
-    			   if(nums[i] < nums[k] && nums[k] < nums[j])
-    				   return true;
-    		   }
-    	   }
-       }
-       
-       return false;
-    }
+		
+		int[] min = new int[nums.length];
+		min[0] = nums[0];
+		for(int i = 1; i < nums.length; i++) min[i] = Integer.min(min[i - 1], nums[i]);
+		
+		Stack<Integer> stk = new Stack<Integer>();
+		
+		for(int i = nums.length - 1; i >= 0; i--) {
+			while(stk.size() > 0 && stk.peek() <= min[i])
+				stk.pop();
+			
+			if(stk.size() > 0 && stk.peek() < nums[i])
+				return true;
+			stk.push(nums[i]);
+		}
+		
+		return false;
+	}
 }
