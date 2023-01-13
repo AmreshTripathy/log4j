e66938aa9a6e4b9be67129313594abb4a9a161a8
AmreshTripathy <amreshtripathy184@gmail.com>
2023-01-13 21:52:05 +0530

    HashMap: Convert To Palindrome https://course.acciojob.com/idle?question=1d8f5733-471c-4eca-916b-323678013a32

---
+++ src/main/java/com/practice/kunalkhuswah_DSA/ConvertToPalindrome.java
@@ -0,0 +1,34 @@
+package com.practice.kunalkhuswah_DSA;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/*
+ * @Amresh Tripathy
+ */
+
+public class ConvertToPalindrome {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String A = sc.nextLine();
+		sc.close();
+		int ans = solve(A);
+		System.out.println(ans);
+	}
+
+	public static int solve(String A) {
+
+		int count = 0;
+
+		int n = A.length();
+		for(int i = 0; i < n / 2;  i++)
+			if(A.charAt(i) != A.charAt(n - i - 1))
+				count++;
+
+		if(count > 1)
+			return 0;
+		
+		return 1;
+	}
+}
