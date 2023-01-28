703ad6b22a1ae84c01f62ad214805b3a723be5ce
AmreshTripathy <amreshtripathy184@gmail.com>
2023-01-29 00:45:07 +0530

    Binary Trees 1: Binary Tree Poostorder Traversal https://course.acciojob.com/idle?question=ac1af0fe-d5d6-4279-b47c-9787423ab256

---
+++ src/main/java/com/practice/kunalkhuswah_DSA/BinaryTreePostorderTraversal.java
@@ -0,0 +1,31 @@
+package com.practice.kunalkhuswah_DSA;
+/*
+ * @Amresh Tripathy
+ * https://course.acciojob.com/idle?question=ac1af0fe-d5d6-4279-b47c-9787423ab256
+ */
+
+import java.util.Scanner;
+
+import com.practice.kunalkhuswah_DSA.BinaryTrees.Node;
+
+public class BinaryTreePostorderTraversal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        String[] arr = str.split(" ");
+        sc.close();
+
+        BinaryTrees bst = new BinaryTrees(arr);
+        postOrderTraversal(bst.root);
+    }
+
+    private static void postOrderTraversal(Node root) {
+        if (root == null)
+            return;
+
+        postOrderTraversal(root.left);
+        postOrderTraversal(root.right);
+        System.out.print(root.data + " ");
+    }
+}
