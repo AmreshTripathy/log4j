4d3f47074d600338fbee08290a28c88401510c48
AmreshTripathy <amreshtripathy184@gmail.com>
2023-01-29 00:43:56 +0530

    Binary Trees 1: Binary Tree Preorder Traversal https://course.acciojob.com/idle?question=1212a47c-5a68-4df2-92b3-768ed753d6eb

---
+++ src/main/java/com/practice/kunalkhuswah_DSA/BinaryTreePreorderTraversal.java
@@ -0,0 +1,30 @@
+package com.practice.kunalkhuswah_DSA;
+/*
+ * @Amresh Tripathy
+ * https://course.acciojob.com/idle?question=1212a47c-5a68-4df2-92b3-768ed753d6eb
+ */
+
+import java.util.Scanner;
+
+import com.practice.kunalkhuswah_DSA.BinaryTrees.Node;
+
+public class BinaryTreePreorderTraversal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String str = sc.nextLine();
+        String[] arr = str.split(" ");
+        sc.close();
+
+        BinaryTrees bst = new BinaryTrees(arr);
+        preorderTraversal(bst.root);
+    }
+
+    private static void preorderTraversal(Node root) {
+        if (root == null)
+            return;
+        System.out.print(root.data + " ");
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+    }
+}
