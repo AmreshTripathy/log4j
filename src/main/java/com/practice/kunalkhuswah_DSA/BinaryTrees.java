d2ffef3042a30bc79a2c612e5b61df00ee311916
AmreshTripathy <amreshtripathy184@gmail.com>
2023-01-28 23:06:52 +0530

    Binary Trees 1: Binary Trees

---
+++ src/main/java/com/practice/kunalkhuswah_DSA/BinaryTrees.java
@@ -0,0 +1,68 @@
+package com.practice.kunalkhuswah_DSA;
+/*
+ * @Amresh Tripathy
+ */
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinaryTrees {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(String data) {
+            this.data = Integer.parseInt(data);
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    Node root = null;
+
+    BinaryTrees(String[] arr) {
+        if(arr.length == 0 || arr[0].equals("N"))
+            return;
+        
+        root = new Node(arr[0]);
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        int i = 1;
+
+        while (q.size() > 0 && i < arr.length) {
+            Node curNode = q.peek();
+            q.remove();
+
+            String curVal = arr[i];
+            if(!curVal.equals("N")) {
+                curNode.left = new Node(curVal);
+                q.add(curNode.left);
+            }
+            i++;
+
+            if (i >= arr.length)
+                break;
+            curVal = arr[i];
+            if(!curVal.equals("N")) {
+                curNode.right = new Node(curVal);
+                q.add(curNode.right);
+            }
+            i++;
+        }
+    }
+
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     int n = sc.nextInt();
+    //     String[] arr = new String[n];
+
+    //     for(int i = 0; i < n; i++) 
+    //         arr[i] = sc.next();
+    //     sc.close();
+        
+    //     BinaryTrees br = new BinaryTrees(arr);
+    //     System.out.println(br.root.data + " " + br.root.left.data + " " + br.root.right.data);
+    // }
+}
