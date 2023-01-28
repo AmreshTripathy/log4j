b1973b79f5ddff020b1be8b091cfe006b9fa86e5
AmreshTripathy <amreshtripathy184@gmail.com>
2023-01-29 00:44:29 +0530

    Binary Trees 1: Binary Tree Inorder Traversal https://course.acciojob.com/idle?question=a6aeb3f2-62db-4840-89f7-7affb74d8643

---
+++ src/main/java/com/practice/kunalkhuswah_DSA/BinaryTreeInorderTraversal.java
@@ -0,0 +1,31 @@
+package com.practice.kunalkhuswah_DSA;
+/*
+ * @Amresh Tripathy
+ * https://course.acciojob.com/idle?question=a6aeb3f2-62db-4840-89f7-7affb74d8643
+ */
+
+import java.util.Scanner;
+
+import com.practice.kunalkhuswah_DSA.BinaryTrees.Node;
+
+public class BinaryTreeInorderTraversal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        String[] arr = str.split(" ");
+        sc.close();
+
+        BinaryTrees bst = new BinaryTrees(arr);
+        inOrderTraversal(bst.root);
+    }
+
+    private static void inOrderTraversal(Node root) {
+        if (root == null)
+            return;
+
+        inOrderTraversal(root.left);
+        System.out.print(root.data + " ");
+        inOrderTraversal(root.right);
+    }
+}
