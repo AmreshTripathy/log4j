f26de2c6ad23fcff7de68c2e0e8b3b8b4cc2ab3f
AmreshTripathy <amreshtripathy184@gmail.com>
2023-02-22 22:07:36 +0530

    Sliding Window and Two Pointers 1: Subarray Product Less Than K https://course.acciojob.com/idle?question=faddb193-7416-4426-8c42-99dcd35916a8

--- src/main/java/com/practice/kunalkhuswah_DSA/SubArrayProductLessThanK.java
+++ src/main/java/com/practice/kunalkhuswah_DSA/SubArrayProductLessThanK.java
@@ -1,23 +1,25 @@
 package com.practice.kunalkhuswah_DSA;
-
-import java.util.Scanner;
-
 /*
  * @Amresh Tripathy
+ * https://course.acciojob.com/idle?question=faddb193-7416-4426-8c42-99dcd35916a8
  */
+
+import java.util.Scanner;
 
 public class SubArrayProductLessThanK {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int k = sc.nextInt();
 		int[] nums = new int[n];
 
 		for (int i = 0; i < n; i++)
 			nums[i] = sc.nextInt();
+
+		int k = sc.nextInt();
 		sc.close();
 
 		System.out.println(numSubarrayProductLessThanK(nums, k));
+		System.out.println(numSubarrayProductLessThanK_TwoPointerApproach(nums, k));
 	}
 
 	public static int numSubarrayProductLessThanK(int[] nums, int k) {
@@ -36,5 +38,25 @@ public class SubArrayProductLessThanK {
 		return count;
 	}
 
-	
+	private static int numSubarrayProductLessThanK_TwoPointerApproach(int[] nums, int k) {
+		int len = 0;
+
+		if (k == 0)
+			return 0;
+
+		int start = 0;
+		int prod = 1;
+
+		for (int i = 0; i < nums.length; i++) {
+			prod *= nums[i];
+
+			while (prod >= k) {
+				prod /= nums[start];
+				start++;
+			}
+
+			len += i - start + 1;
+		}
+		return len;
+	}
 }
