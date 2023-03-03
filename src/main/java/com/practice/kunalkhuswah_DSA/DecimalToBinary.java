66aae9a8a6edc34a26438e9473b8cfe15415761c
AmreshTripathy <amreshtripathy184@gmail.com>
2023-03-03 23:05:42 +0530

    Decimal to Binary: Adding reverse string statement

--- src/main/java/com/practice/kunalkhuswah_DSA/DecimalToBinary.java
+++ src/main/java/com/practice/kunalkhuswah_DSA/DecimalToBinary.java
@@ -27,7 +27,7 @@ public class DecimalToBinary {
                 ans += '0';
             N >>= 1;
         }
-
+        ans = (new StringBuilder(ans)).reverse().toString();
         System.out.println(ans);
 	}
 }
